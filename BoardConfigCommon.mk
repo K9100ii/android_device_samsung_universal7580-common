a8744fd4e66615665c0eb34a1862e2b702f27f36
Roger Ellis <ellisr@google.com>
2020-05-13 16:56:10 -0700

    universal7580: Add exclusion for vendor ELF prebuilts
     - ELF prebuilts were banned in PRODUCT_COPY_FILES starting May 11.
     - Adding BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true to
    BoardConfig.mk temporarily.
    
    Test: Build
    Bug: 156534160
    Change-Id: I7d80798d3b2d3818bc835fcede21de2e7ab04735

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,6 +18,7 @@ LOCAL_PATH := device/samsung/universal7580-common
 BUILD_TOP := $(shell pwd)
 
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Include path
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
