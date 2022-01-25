63053c239563477126b53485f3105d7e11b304e4
TALU <taluemail272@gmail.com>
2022-01-25 01:47:02 +0000

    universal7580: Switch to the 1.3 Samsung vibrator HAL from hardware/samsung
    Change-Id: I1f2a6b7bcb3bf255d57e4fcc0324810cd56fcc0d

--- device-common.mk
+++ device-common.mk
@@ -164,8 +164,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.3-service.samsung
 
 # Shims
 PRODUCT_PACKAGES += \
