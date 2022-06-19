38a2260f04c1f54a2835c64541f65045bf2342fa
TALU <taluemail272@gmail.com>
2022-06-19 21:53:27 +0100

    universal7580: Prefix the audio_hal.force_voice_config property with 'ro.vendor.'
    as per the latest change to the audio HAL.
    
    Change-Id: Ibe2f1a11a559a6e030639d4f4ce8e66603609535

--- system_prop.mk
+++ system_prop.mk
@@ -17,7 +17,7 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
-    audio_hal.force_voice_config=wide \
+    ro.vendor.audio_hal.force_voice_config=wide \
     ro.config.media_vol_steps=25 \
     ro.config.vc_call_vol_steps=7
 
