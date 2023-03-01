53920e8d405de7d61dfeb24a5aee7803b5c323fb
Luofan Chen <clfbbn@gmail.com>
2023-03-01 20:51:49 +0800

    universal7580: Build lineage health HAL
    Change-Id: I1ca5f25f3a2d8734647b9cc0ff4885b0b817c6ad

--- device-common.mk
+++ device-common.mk
@@ -113,6 +113,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.samsung
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
